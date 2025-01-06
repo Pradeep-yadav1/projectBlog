073dcd736fc9dd16270b8ae6e6e05bfb1b9631ab
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-06 21:12:35 +0545

    all backend logics done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,7 @@
 import { Hono } from "hono";
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-import { env } from "hono/adapter";
-import { decode, jwt, sign, verify } from "hono/jwt";
-import bcrypt from "bcryptjs";
+
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 
 const app = new Hono<{
@@ -16,79 +14,7 @@ const app = new Hono<{
   }
 }>();
 
-app.use("/api/v1/blog/*", async (c, next) => {
-  const authHeader = c.req.header("Authorization");
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return c.json({ msg: "UnAuthorized : missing valid token" }, 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-  const payload= await verify(token, c.env.JWT_SECRET);
-  if (!payload.id) {
-    return c.json({ msg: "unauthorized access" }, 403);
-  }
-//@ts-ignore
-  c.set("userId", payload.id);
-  await next();
-});
-
-app.post("/api/v1/signup", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const saltRounds = 10;
-  const hashPassword = await bcrypt.hash(body.password, saltRounds);
-  const user = await prisma.user.create({
-    data: {
-      name: body.name,
-      email: body.email,
-      password: hashPassword,
-    },
-  });
-
-  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({ msg: "user created successfully", token: token });
-});
-
-app.post("/api/v1/signin", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-      password: body.password,
-    },
-  });
-  if (!user) {
-    return c.json({ msg: "User not found" }, 403);
-  }
-  const isPasswordValid = await bcrypt.compare(body.password, user.password);
-  if (!isPasswordValid) {
-    c.status(411);
-    return c.json({
-      msg: "Invalid password",
-    });
-  }
-  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({
-    msg: "Signin successfully",
-    token: token,
-  });
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.json("Hello Hono!");
-});
-app.put("/api/v1/blog/", (c) => {
-  return c.text("Hello Hono!");
-});
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("Hello Hono!");
-});
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/blog', blogRouter)
 
 export default app;
