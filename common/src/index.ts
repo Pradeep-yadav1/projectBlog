e0fd6fdfd4bef783e74ae821aa7ffb8e90f6a189
Pradeep yadav <pradeep20020102@gmail.com>
2025-01-07 14:01:25 +0545

    zod validation common package created and deployed

---
+++ common/src/index.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const signupInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(4),
+    name:z.string().optional()
+})
+
+export const signinInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(4),
+})
+
+export const createBlogInput = z.object({
+    title:z.string(),
+    content:z.string(),
+    published:z.boolean()
+})
+
+export const updateBlogInput = z.object({
+    id:z.string(),
+    title:z.string(),
+    content:z.string(),
+    published:z.boolean().optional()
+})
+
+export const specificBlogInput = z.object({
+   id:z.string()
+})
+
+export type SignupInput = z.infer<typeof signupInput>
+export type SigninInput = z.infer<typeof signinInput>
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+export type updateBlogInput = z.infer<typeof updateBlogInput>
+export type SpecificBlogInput = z.infer<typeof specificBlogInput>
